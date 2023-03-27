d08acbdf41763b845ad5dccb67587826163a5ebb
wasitthat <wasitthat@zohomail.com>
2023-03-27 00:29:42 -0700

    updated responsiveness of several elements

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php include "./header.html" ?>
+    <div id="wtf">
+        <iframe width="800px"
+                height="450px"
+                src="https://www.youtube.com/embed/gFF2mZJ-EL4"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                class="video"
+                allowfullscreen>
+        </iframe>
+    </div>
+    <hr class="hr2">
+    <div id="wwy">
+        <iframe width="800px"
+                height="450px"
+                src="https://www.youtube.com/embed/GflrvecFA7o"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                class="video"
+                allowfullscreen>
+        </iframe>
+    </div>
+    <footer id="footer" class="">
+        <div class="icons">
+            <a href="https://www.facebook.com/Inertiametal"
+               class = "icons fa-brands fa-facebook"
+               target="_blank">
+            </a>
+            <a href="https://www.instagram.com/Inertiametal/"
+               class = "icons fa-brands fa-instagram"
+               target="_blank">
+            </a>
+            <a href="https://www.youtube.com/user/wasitthat"
+               class = "icons fa-brands fa-youtube"
+               target="_blank">
+            </a>
+            <a href="mailto:wasitthat@zohomail.com"
+               class = "icons fa-solid fa-envelope">
+            </a>
+        </div>
+        <p class = "navbar-brand">© Copyright 2022 Inertia</p>
+    </footer>
+</body>
+</html>
---
+++ links.php
@@ -0,0 +1,33 @@
+<?php include "header.html" ?>
+
+    <section class="about">
+    <p>
+        Inertia was founded in Southern California in early 2002 with Vincent Fernandez, John Oden and Arthur Rodriguez.
+        Playing with reckless abandon and with a zeal for attention, Inertia quickly became the local mosh-able must-see
+        metal group around, playing original bone crushing tunes such as 'Dun Dun Dun Dun' and 'Where Were You?'.
+    </p>
+    <p>Inertia is currently in the studio working on their long awaited full length record.</p>
+    </section>
+    <hr class="hr2">
+    <footer id="footer" class="">
+        <div class="icons">
+            <a href="https://www.facebook.com/Inertiametal"
+               class = "icons fa-brands fa-facebook"
+               target="_blank">
+            </a>
+            <a href="https://www.instagram.com/Inertiametal/"
+               class = "icons fa-brands fa-instagram"
+               target="_blank">
+            </a>
+            <a href="https://www.youtube.com/user/wasitthat"
+               class = "icons fa-brands fa-youtube"
+               target="_blank">
+            </a>
+            <a href="mailto:wasitthat@zohomail.com"
+               class = "icons fa-solid fa-envelope">
+            </a>
+        </div>
+        <p class = "navbar-brand">© Copyright 2022 Inertia</p>
+    </footer>
+</body>
+</html>
