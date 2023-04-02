a06cfda4fef1e45b522336bc22b436d89a31a50b
wasitthat <wasitthat@zohomail.com>
2023-04-02 01:46:52 -0700

    updated from .net

--- index.php
+++ index.php
@@ -1,46 +1,45 @@
 <?php include "./header.html" ?>
-    <div id="wtf">
-        <iframe width="800px"
-                height="450px"
-                src="https://www.youtube.com/embed/gFF2mZJ-EL4"
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                class="video"
-                allowfullscreen>
+<section id="vids">
+<div class="vid">
+    <iframe
+    width: 800px;
+    height: 450px;
+    
+    src="https://www.youtube.com/embed/EgdOO50jz9A" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        class="video" 
+        allowfullscreen>
+    </iframe>
+</div>
+<img src="./static/css/images/border.png" class="hr1">
+    <div class="vid">
+        <iframe 
+            width: 800px;
+    height: 450px;
+    
+            src="https://www.youtube.com/embed/gFF2mZJ-EL4"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            class="video"
+            allowfullscreen>
         </iframe>
     </div>
-    <hr class="hr2">
-    <div id="wwy">
-        <iframe width="800px"
-                height="450px"
-                src="https://www.youtube.com/embed/GflrvecFA7o"
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                class="video"
-                allowfullscreen>
+ <img src="./static/css/images/border.png" class="hr1">
+    <div class="vid">
+        <iframe 
+            width: 800px;
+    height: 450px;
+    
+            src="https://www.youtube.com/embed/GflrvecFA7o"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            class="video"
+            allowfullscreen>
         </iframe>
     </div>
-    <footer id="footer" class="">
-        <div class="icons">
-            <a href="https://www.facebook.com/Inertiametal"
-               class = "icons fa-brands fa-facebook"
-               target="_blank">
-            </a>
-            <a href="https://www.instagram.com/Inertiametal/"
-               class = "icons fa-brands fa-instagram"
-               target="_blank">
-            </a>
-            <a href="https://www.youtube.com/user/wasitthat"
-               class = "icons fa-brands fa-youtube"
-               target="_blank">
-            </a>
-            <a href="mailto:wasitthat@zohomail.com"
-               class = "icons fa-solid fa-envelope">
-            </a>
-        </div>
-        <p class = "navbar-brand">© Copyright 2022 Inertia</p>
-    </footer>
-</body>
-</html>
+    </section>
+<?php include "footer.html" ?>
